8b1718e8055067c5f6738939ea5a8eda110eddcf
Lucas Braga <73352127+assuncaolucasss@users.noreply.github.com>
2026-04-28 16:51:01 -0300

    Add function to create database and tables
    Added create_db_and_tables function to initialize database tables.

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, SQLModel
 from sqlalchemy.pool import NullPool
 import os
 
@@ -6,9 +6,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    poolclass=NullPool,  # obrigatório para Neon serverless
+    poolclass=NullPool,
 )
 
 def get_session():
     with Session(engine) as session:
         yield session
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
