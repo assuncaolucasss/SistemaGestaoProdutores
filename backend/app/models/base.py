73768caefd6eaa36995867f81124c28347883286
Lucas Braga <bragaluka19@gmail.com>
2026-04-28 09:36:49 -0300

    Normalização para deploy no render

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,10 +1,13 @@
-from sqlmodel import SQLModel, create_engine, Session
-from app.core.config import settings
+from sqlmodel import create_engine, Session
+from sqlalchemy.pool import NullPool
+import os
 
-engine = create_engine(settings.DATABASE_URL, echo=False)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=NullPool,  # obrigatório para Neon serverless
+)
 
 def get_session():
     with Session(engine) as session:
