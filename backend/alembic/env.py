650cb68db1fce193d7a2a53778e98d81c0969ec2
Lucas Braga <73352127+assuncaolucasss@users.noreply.github.com>
2026-09-14 18:32:10 +0000

    add alembic migrations configuration

---
+++ backend/alembic/env.py
@@ -0,0 +1,56 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from alembic import context
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from sqlmodel import SQLModel
+from app.models.base import get_session
+from app.models import *  # importa TODOS os modelos
+
+target_metadata = SQLModel.metadata
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+
+def run_migrations_offline():
+    context.configure(
+        connection=context.config.attributes.get("connection", None),
+        target_metadata=target_metadata,
+        render_as_batch=True,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True,
+            compare_type=True,
+        )
+
+        with context.begin_transaction():
+            connection.execute("SELECT 1")
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
